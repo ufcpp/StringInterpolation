ea2ec96f22bbcd87abcf45300eb521afe00f3f0e
Nobuyuki Iwanaga <ufcpp@live.jp>
2023-01-08 18:52:50 +0900

    Join<T>() where T : ISpanFormattable

---
+++ src/StringInterpolation/Format.JoinT.cs
@@ -0,0 +1,38 @@
+﻿namespace StringInterpolation;
+
+public static partial class Format
+{
+    /// <summary>
+    /// Joins <see cref="ISpanFormattable"/>s without temprary string allocation.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="Join(string, IEnumerable{string})"/>
+    /// </remarks>
+    public static JoinFormattable<T> Join<T>(string separator, IEnumerable<T> values)
+        where T : ISpanFormattable
+        => new(separator, values);
+
+    /// <summary>
+    /// <see cref="Join{T}(string, IEnumerable{T})"/>
+    /// </summary>
+    public readonly struct JoinFormattable<T> : IBuilderFormattable
+        where T : ISpanFormattable
+    {
+        public readonly string Separator;
+        public readonly IEnumerable<T> Values;
+        public JoinFormattable(string separator, IEnumerable<T> values) => (Separator, Values) = (separator, values);
+
+        public bool Format(scoped SpanStringBuilder builder, ReadOnlySpan<char> format)
+        {
+            bool part = false;
+            foreach (var value in Values)
+            {
+                if (!part) part = true;
+                else if (!builder.Append(Separator)) return false;
+
+                if (!builder.Append(value, format)) return false;
+            }
+            return true;
+        }
+    }
+}
--- test/StringInterpolationTest/JoinTest.cs
+++ test/StringInterpolationTest/JoinTest.cs
@@ -1,4 +1,6 @@
-﻿namespace StringInterpolationTest;
+﻿using System.Globalization;
+
+namespace StringInterpolationTest;
 
 public class JoinTest
 {
@@ -16,5 +18,70 @@ public class JoinTest
         Assert.Equal(
             string.Join(" / ", Enumerable.Repeat("123456789abcdefghijk", 100)),
             $"{Format.Join(" / ", Enumerable.Repeat("123456789abcdefghijk", 100))}");
+    }
+
+    [Fact]
+    public void JoinT()
+    {
+        Assert.Equal(
+            "1,12,123,1234",
+            $"{Format.Join(",", new[] { 1, 12, 123, 1234 })}");
+
+        Assert.Equal(
+            "1000, 1000000, 1000000000, 1000000000000",
+            $"{Format.Join(", ", new long[] { 1000, 1000000, 1000000000, 1000000000000 })}");
+
+        Assert.Equal(
+            string.Join(" / ", Enumerable.Repeat(123456789, 100)),
+            $"{Format.Join(" / ", Enumerable.Repeat(123456789, 100))}");
+    }
+
+    [Fact]
+    public void JoinWithCulture()
+    {
+        var jp = CultureInfo.GetCultureInfo("ja-jp");
+        var fr = CultureInfo.GetCultureInfo("fr-fr");
+
+        var nums = new[] { 1.2, 1.5, 1.7 };
+        var dates = new[] { new DateOnly(2000, 1, 2), new DateOnly(2020, 12, 31) };
+
+        Assert.Equal(
+            "1.2/1.5/1.7",
+            string.Create(jp, $"{Format.Join("/", nums)}"));
+
+        Assert.Equal(
+            "1,2/1,5/1,7",
+            string.Create(fr, $"{Format.Join("/", nums)}"));
+
+        Assert.Equal(
+            "2000/01/02, 2020/12/31",
+            string.Create(jp, $"{Format.Join(", ", dates)}"));
+
+        Assert.Equal(
+            "02/01/2000, 31/12/2020",
+            string.Create(fr, $"{Format.Join(", ", dates)}"));
+    }
+
+    [Fact]
+    public void JoinWithFormat()
+    {
+        var nums = new[] { 5, 10, 15, 20 };
+        var dates = new[] { new DateOnly(2000, 1, 2), new DateOnly(2020, 12, 31) };
+
+        Assert.Equal(
+            "5/10/15/20",
+            $"{Format.Join("/", nums):d}");
+
+        Assert.Equal(
+            "5/a/f/14",
+            $"{Format.Join("/", nums):x}");
+
+        Assert.Equal(
+            "05/0A/0F/14",
+            $"{Format.Join("/", nums):X2}");
+
+        Assert.Equal(
+            "01-2000, 12-2020",
+            $"{Format.Join(", ", dates):MM-yyyy}");
     }
 }
