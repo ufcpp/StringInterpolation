10bde5ec7b2c8f7340ae93eb5aeee50afe53503d
Nobuyuki Iwanaga <ufcpp@live.jp>
2023-01-08 21:13:29 +0900

    ForEach

---
+++ src/StringInterpolation/Format.ForEach.cs
@@ -0,0 +1,118 @@
+﻿namespace StringInterpolation;
+
+public static partial class Format
+{
+    /// <summary>
+    /// </summary>
+    /// <example>
+    /// $"... {ForEach(values, (value, builder) => builder.Append($"{value}")) } ..."
+    /// </example>
+    public static ForEachBuilder<TValue> ForEach<TValue>(IEnumerable<TValue> values, ForEachAction<TValue> action) => new(values, action);
+
+    /// <summary>
+    /// <see cref="ForEach{TValue, T}(IEnumerable{TValue}, T, ForEachAction{TValue, T})"/>
+    /// </summary>
+    public delegate bool ForEachAction<TValue>(TValue value, SpanStringBuilder builder);
+
+    /// <summary>
+    /// <see cref="ForEach{TValue}(IEnumerable{TValue}, ForEachAction{TValue})"/>
+    /// </summary>
+    public readonly struct ForEachBuilder<TValue> : IBuilderFormattable
+    {
+        private readonly IEnumerable<TValue> _values;
+        private readonly ForEachAction<TValue> _action;
+        public ForEachBuilder(IEnumerable<TValue> values, ForEachAction<TValue> action) => (_values, _action) = (values, action);
+        public bool Format(scoped SpanStringBuilder builder, ReadOnlySpan<char> format)
+        {
+            foreach (var value in _values)
+                if (!_action(value, builder)) return false;
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <example>
+    /// $"... {ForEach(values, state, (value, state, builder) => builder.Append($"{value} {state}")) } ..."
+    /// </example>
+    public static ForEachBuilder<TValue, TState> ForEach<TValue, TState>(IEnumerable<TValue> values, TState state, ForEachAction<TValue, TState> action) => new(values, state, action);
+
+    /// <summary>
+    /// <see cref="ForEach{TValue, TState}(IEnumerable{TValue}, TState, ForEachAction{TValue, TState})"/>
+    /// </summary>
+    public delegate bool ForEachAction<TValue, TState>(TValue value, TState state, SpanStringBuilder builder);
+
+    /// <summary>
+    /// <see cref="ForEach{TValue, TState}(IEnumerable{TValue}, TState, ForEachAction{TValue, TState})"/>
+    /// </summary>
+    public readonly struct ForEachBuilder<TValue, TState> : IBuilderFormattable
+    {
+        private readonly IEnumerable<TValue> _values;
+        private readonly ForEachAction<TValue, TState> _action;
+        private readonly TState _state;
+        public ForEachBuilder(IEnumerable<TValue> values, TState state, ForEachAction<TValue, TState> action) => (_values, _action, _state) = (values, action, state);
+        public bool Format(scoped SpanStringBuilder builder, ReadOnlySpan<char> format)
+        {
+            foreach (var value in _values)
+                if (!_action(value, _state, builder)) return false;
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <example>
+    /// $"... {ForEach(values, (value, builder, format) => builder.Append($"{value}")) } ..."
+    /// </example>
+    public static FormatForEachBuilder<TValue> ForEach<TValue>(IEnumerable<TValue> values, FormatForEachAction<TValue> action) => new(values, action);
+
+    /// <summary>
+    /// <see cref="ForEach{TValue}(IEnumerable{TValue}, FormatForEachAction{TValue})"/>
+    /// </summary>
+    public delegate bool FormatForEachAction<TValue>(TValue value, SpanStringBuilder builder, ReadOnlySpan<char> format);
+
+    /// <summary>
+    /// <see cref="ForEach{TValue}(IEnumerable{TValue}, FormatForEachAction{TValue})"/>
+    /// </summary>
+    public readonly struct FormatForEachBuilder<TValue> : IBuilderFormattable
+    {
+        private readonly IEnumerable<TValue> _values;
+        private readonly FormatForEachAction<TValue> _action;
+        public FormatForEachBuilder(IEnumerable<TValue> values, FormatForEachAction<TValue> action) => (_values, _action) = (values, action);
+        public bool Format(scoped SpanStringBuilder builder, ReadOnlySpan<char> format)
+        {
+            foreach (var value in _values)
+                if (!_action(value, builder, format)) return false;
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <example>
+    /// $"... {ForEach(values, state, (value, state, builder, format) => builder.Append($"{value} {state}")) } ..."
+    /// </example>
+    public static FormatForEachBuilder<TValue, TState> ForEach<TValue, TState>(IEnumerable<TValue> values, TState state, FormatForEachAction<TValue, TState> action) => new(values, state, action);
+
+    /// <summary>
+    /// <see cref="ForEach{TValue, TState}(IEnumerable{TValue}, TState, FormatForEachAction{TValue, TState})"/>
+    /// </summary>
+    public delegate bool FormatForEachAction<TValue, TState>(TValue value, TState state, SpanStringBuilder builder, ReadOnlySpan<char> format);
+
+    /// <summary>
+    /// <see cref="ForEach{TValue, TState}(IEnumerable{TValue}, TState, FormatForEachAction{TValue, TState})"/>
+    /// </summary>
+    public readonly struct FormatForEachBuilder<TValue, TState> : IBuilderFormattable
+    {
+        private readonly IEnumerable<TValue> _values;
+        private readonly FormatForEachAction<TValue, TState> _action;
+        private readonly TState _state;
+        public FormatForEachBuilder(IEnumerable<TValue> values, TState state, FormatForEachAction<TValue, TState> action) => (_values, _action, _state) = (values, action, state);
+        public bool Format(scoped SpanStringBuilder builder, ReadOnlySpan<char> format)
+        {
+            foreach (var value in _values)
+                if (!_action(value, _state, builder, format)) return false;
+            return true;
+        }
+    }
+}
---
+++ test/StringInterpolationTest/ForEachTest.cs
@@ -0,0 +1,183 @@
+﻿using System.Globalization;
+using static StringInterpolation.Format;
+
+namespace StringInterpolationTest;
+
+public class ForEachTest
+{
+    [Fact]
+    public void NoArg()
+    {
+        Assert.Equal($"""
+            [
+                1,
+                2,
+                3,
+                4,
+            ]
+            """,
+            $"""
+            [
+            {ForEach(new[] { 1, 2, 3, 4 }, static (v, b) => b.Append($"""
+                {v},
+
+            """))}]
+            """);
+
+        Assert.Equal($"""
+            [
+                1.1,
+                1.2,
+                1.3,
+                1.4,
+            ]
+            """,
+           string.Create(CultureInfo.InvariantCulture, $"""
+            [
+            {ForEach(new[] { 1.1, 1.2, 1.3, 1.4 }, static (v, b) => b.Append($"""
+                {v},
+
+            """))}]
+            """));
+
+        Assert.Equal($"""
+            [
+                1,1,
+                1,2,
+                1,3,
+                1,4,
+            ]
+            """,
+           string.Create(CultureInfo.GetCultureInfo("fr-fr"), $"""
+            [
+            {ForEach(new[] { 1.1, 1.2, 1.3, 1.4 }, static (v, b) => b.Append($"""
+                {v},
+
+            """))}]
+            """));
+    }
+
+    [Fact]
+    public void State()
+    {
+        Assert.Equal($"""
+            [
+                5,
+                10,
+                15,
+                20,
+            ]
+            """,
+            $"""
+            [
+            {ForEach(new[] { 1, 2, 3, 4 }, 5, static (v, s, b) => b.Append($"""
+                {v * s},
+
+            """))}]
+            """);
+
+        Assert.Equal($"""
+            [
+                1.25,
+                2.5,
+                3.75,
+                5,
+            ]
+            """,
+           string.Create(CultureInfo.InvariantCulture, $"""
+            [
+            {ForEach(new[] { 1, 2, 3, 4 }, 1.25, static (v, s, b) => b.Append($"""
+                {v * s},
+
+            """))}]
+            """));
+
+        Assert.Equal($"""
+            [
+                1,25,
+                2,5,
+                3,75,
+                5,
+            ]
+            """,
+           string.Create(CultureInfo.GetCultureInfo("fr-fr"), $"""
+            [
+            {ForEach(new[] { 1, 2, 3, 4 }, 1.25, static (v, s, b) => b.Append($"""
+                {v * s},
+
+            """))}]
+            """));
+    }
+
+    [Fact]
+    public void Format()
+    {
+        Assert.Equal($"""
+            [
+                5,
+                10,
+                15,
+                20,
+            ]
+            """,
+            $"""
+            [
+            {ForEach(new[] { 5, 10, 15, 20 }, static (v, b) => b.Append($"""
+                {v},
+
+            """)):X}]
+            """);
+
+        Assert.Equal($"""
+            [
+                5,
+                A,
+                F,
+                14,
+            ]
+            """,
+            $"""
+            [
+            {ForEach(new[] { 5, 10, 15, 20 }, static (v, b, f) => b.Append($"""
+                {v.ToString(f.ToString(), null)},
+
+            """)):X}]
+            """);
+    }
+
+    [Fact]
+    public void StateFormat()
+    {
+        Assert.Equal($"""
+            [
+                5,
+                10,
+                15,
+                20,
+            ]
+            """,
+            $"""
+            [
+            {ForEach(new[] { 1, 2, 3, 4 }, 5, static (v, s, b) => b.Append($"""
+                {v * s},
+
+            """)):X}]
+            """);
+
+        Assert.Equal($"""
+            [
+                5,
+                A,
+                F,
+                14,
+            ]
+            """,
+            $"""
+            [
+            {ForEach(new[] { 1, 2, 3, 4 }, 5, static (v, s, b, f) => b.Append($"""
+                {(v * s).ToString(f.ToString(), null)},
+
+            """)):X}]
+            """);
+    }
+}
