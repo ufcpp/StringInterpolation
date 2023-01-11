8cbfed07848249786a6c809db99f509e7571fe86
Nobuyuki Iwanaga <ufcpp@live.jp>
2023-01-11 22:59:42 +0900

    If

---
+++ src/StringInterpolation/Format.If.cs
@@ -0,0 +1,50 @@
+﻿namespace StringInterpolation;
+
+public static partial class Format
+{
+    /// <summary>
+    /// </summary>
+    /// <example>
+    /// $"... {
+    ///     If(condition,
+    ///         builder => builder.Append($"then {...}")
+    ///     ) } ..."
+    /// </example>
+    public static DoBuilder If(bool predicate, DoAction thenAction)
+        => predicate ? new(thenAction) : new(_ => true);
+
+    /// <summary>
+    /// </summary>
+    /// <example>
+    /// $"... {
+    ///     If(condition,
+    ///         builder => builder.Append($"then {...}"),
+    ///         builder => builder.Append($"else {...}")
+    ///     ) } ..."
+    /// </example>
+    public static DoBuilder If(bool predicate, DoAction thenAction, DoAction elseAction)
+        => predicate ? new(thenAction) : new(elseAction);
+
+    /// <summary>
+    /// </summary>
+    /// <example>
+    /// $"... {
+    ///     If(condition, state,
+    ///         (state, builder) => builder.Append($"then {...}")
+    ///     ) } ..."
+    /// </example>
+    public static DoBuilder<TState> If<TState>(bool predicate, TState state, DoAction<TState> thenAction)
+        => predicate ? new(state, thenAction) : new(default!, (_, _) => true);
+
+    /// <summary>
+    /// </summary>
+    /// <example>
+    /// $"... {
+    ///     If(condition, state,
+    ///         (state, builder) => builder.Append($"then {...}"),
+    ///         (state, builder) => builder.Append($"else {...}")
+    ///     ) } ..."
+    /// </example>
+    public static DoBuilder<TState> If<TState>(bool predicate, TState state, DoAction<TState> thenAction, DoAction<TState> elseAction)
+        => predicate ? new(state, thenAction) : new(state, elseAction);
+}
---
+++ test/StringInterpolationTest/IfTest.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using static StringInterpolation.Format;
+
+namespace StringInterpolationTest;
+
+public class IfTest
+{
+    [Fact]
+    public void NoArg()
+    {
+        var b = new StringBuilder();
+
+        for (int i = 0; i < 10; i++)
+        {
+            b.Append($"({If((i & 1) == 0,
+                b => b.Append($"[{i}]")
+                )})");
+        }
+
+        Assert.Equal("([0])()([2])()([4])()([6])()([8])()", b.ToString());
+    }
+
+    [Fact]
+    public void NoArgElse()
+    {
+        var b = new StringBuilder();
+
+        for (int i = 0; i < 10; i++)
+        {
+            b.Append($"({If((i & 1) == 0,
+                b => b.Append($"[{i}]"),
+                b => b.Append($"<{i}>")
+                )})");
+        }
+
+        Assert.Equal("([0])(<1>)([2])(<3>)([4])(<5>)([6])(<7>)([8])(<9>)", b.ToString());
+    }
+
+    [Fact]
+    public void State()
+    {
+        var b = new StringBuilder();
+
+        for (int i = 0; i < 10; i++)
+        {
+            b.Append($"({If((i & 1) == 0, i,
+                static (i, b) => b.Append($"[{i}]")
+                )})");
+        }
+
+        Assert.Equal("([0])()([2])()([4])()([6])()([8])()", b.ToString());
+    }
+
+    [Fact]
+    public void StateElse()
+    {
+        var b = new StringBuilder();
+
+        for (int i = 0; i < 10; i++)
+        {
+            b.Append($"({If((i & 1) == 0, i,
+                static (i, b) => b.Append($"[{i}]"),
+                static (i, b) => b.Append($"<{i}>")
+                )})");
+        }
+
+        Assert.Equal("([0])(<1>)([2])(<3>)([4])(<5>)([6])(<7>)([8])(<9>)", b.ToString());
+    }
+}
